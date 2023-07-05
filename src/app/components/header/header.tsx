6beaa061ece55e2c1a8a18147bf0e3f16190ee94
mzparacha <malik.zulqurnain2121@gmail.com>
2023-07-05 17:57:53 +0500

    little code cleanup

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -6,26 +6,38 @@ import { HiOutlineShoppingBag, HiOutlineHeart, HiOutlineUser } from "react-icons
 import SiteLogo from "@/assets/icons/logo.svg";
 import HeaderGenderList from "./headerGenderList";
 import SubHeader from "./subheader";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="w-full py-1 md:py-3">
       <div className="w-full h-[68px] flex flex-row justify-between items-center">
+        {/* HEADER TOP LEFT */}
         <div className="hidden md:flex md:flex-1">
-          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100 rounded-l-md cursor-pointer">Women</div>
-          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100 cursor-pointer">Men</div>
-          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100 cursor-pointer">Kids</div>
-          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100 rounded-r-md cursor-pointer">Beauty</div>
+          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100 rounded-l-md">
+            <Link href="#">Women</Link>
+          </div>
+          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100">
+            <Link href="#">Men</Link>
+          </div>
+          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100">
+            <Link href="#">Kids</Link>
+          </div>
+          <div className="text-base px-[12px] py-[10px] hover:bg-zinc-100 rounded-r-md">
+            <Link href="#">Beauty</Link>
+          </div>
         </div>
 
         <div className="flex-1 md:hidden">
           <HeaderGenderList />
         </div>
 
+        {/* SITE LOGO */}
         <div className="flex flex-1 justify-center">
           <Image src={SiteLogo} alt="site-logo" className="h-4 w-auto md:h-6 md:w-auto" />
         </div>
 
+        {/* HEADER TOP RIGHT */}
         <div className="flex flex-1 justify-end">
           <div className="p-3 hover:bg-zinc-100 rounded-lg cursor-pointer">🇺🇸</div>
           <div className="p-3 hover:bg-zinc-100 rounded-lg cursor-pointer">
@@ -40,6 +52,7 @@ const Header = () => {
         </div>
       </div>
 
+      {/* SUB-HEADER SECTION */}
       <div className="w-full h-[44px] flex flex-row justify-between gap-4">
         <SubHeader />
 
